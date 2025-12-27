f35c54d68d6a0bb79d841b384004b4157df982d5
Shubham Badresiya <your_email@example.com>
2025-12-27 12:59:53 +0530

    add initial project structure with database connection and response utilities

---
+++ config/database.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+
+load_dotenv()
+
+MONGO_DETAILS = os.getenv("MONGO_URI")
+
+client = AsyncIOMotorClient(MONGO_DETAILS)
+database = client["ai-interview-platform"]
+
+@asynccontextmanager
+async def lifespan(app):
+    print("✅ Connected to MongoDB!")
+    yield
+    client.close()
+    print("❌ MongoDB connection closed.")
+
